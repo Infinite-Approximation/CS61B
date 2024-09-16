b7fb8159ae86b9b03244e2a5ff9e9b48dce0edda
jkd <2310572998jin@gmail.com>
2024-09-16 10:45:32 +0800

    Project1 Extra Credit

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,2 +1,39 @@
-package tester;public class TestArrayDequeEC {
+package tester;
+
+import static org.junit.Assert.*;
+
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+    /**
+     * @source: AssertEqualsStringDemo.java
+     */
+    @Test
+    public void test1() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
+        String s = "";
+        int N = 5000;
+        for (int i = 0; i < 5000; i += 1) {
+            double operationNum = StdRandom.uniform(0, 4);
+            if (operationNum == 0) {
+                sad1.addLast(i);
+                sad2.addLast(i);
+                s = s + "addLast(" + i + ")" + "\n";
+            } else if (operationNum == 1){
+                sad1.addFirst(i);
+                sad2.addFirst(i);
+                s = s + "addFirst(" + i + ")" + "\n";
+            } else if (operationNum == 2 && !sad2.isEmpty()){
+                s = s + "removeLast()" + "\n";
+                assertEquals(s, sad2.removeLast(), sad1.removeLast());
+            } else if (operationNum == 3 && !sad2.isEmpty()){
+                s = s + "removeLast()" + "\n";
+                assertEquals(s, sad2.removeFirst(), sad1.removeFirst());
+            }
+        }
+    }
 }
